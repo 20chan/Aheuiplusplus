80f921a8b2040b086a6c994fd9f42ff6dcb70888
philly <sonagi.philly@gmail.com>
2016-12-13 20:14:06 +0900

    Add CodeEditor usercontrol

--- Ageui++/MainForm.cs
+++ Ageui++/MainForm.cs
@@ -6,14 +6,18 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Aheui__;
 
 namespace Ageui__
 {
     public partial class MainForm : Form
     {
+        List<string> _openedFiles;
         public MainForm()
         {
             InitializeComponent();
+            _openedFiles = new List<string>();
         }
 
         public MainForm(string file) : this()
@@ -21,14 +25,45 @@ namespace Ageui__
             Open(file);
         }
 
+        private void Error(string text)
+        {
+            MessageBox.Show(text, "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
         private void Open(string file)
         {
-            
+            if(!File.Exists(file))
+            {
+                Error("파일이 존재하지 않습니다!");
+                return;
+            }
+            if(_openedFiles.Contains(file))
+            {
+                Error("이미 파일이 열려있습니다!");
+                return;
+            }
+
+            _openedFiles.Add(file);
+
+        }
+
+        private void AddTabPage(string file)
+        {
+            CodeEditor c = new CodeEditor(File.ReadAllText(file));
+            c.Dock = DockStyle.Fill;
+            TabPage t = new TabPage(Path.GetFileName(file));
+            t.Controls.Add(c);
+            this.tabControl1.TabPages.Add(t);
+        }
+
+        private string GetCurrentScript()
+        {
+            throw new NotImplementedException();
         }
 
         private void Run()
         {
-
+            IntAheui aheui = new IntAheui(GetCurrentScript(), false);
         }
 
         private void StartDebug()
